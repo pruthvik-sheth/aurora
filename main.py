53f0fa1d2b5481b7be7f15d0d61c75641f299f4b
Pruthvik <pns00911@gmail.com>
2023-11-06 12:18:21 +0530

    minor fixes

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from light import Light
 
 
 def main():
-    WIDTH = 500
-    HEIGHT = 375 #4:3 ratio
+    WIDTH = 800
+    HEIGHT = 600  # 4:3 ratio
 
     window = pg.display.set_mode((WIDTH, HEIGHT))
     pg.display.set_caption("Ray Tracer!")
@@ -18,17 +18,16 @@ def main():
     camera = Vector(0, 0, 2)
     objects = [
         # Sphere(Vector(-1, 0, -2), 0.5, Color.from_hex("#ff0000")),
-        Sphere(Vector(0, 0, 0), 1.0, Material(Color.from_hex("#00ffff"), ambient=1.0)),
+        Sphere(Vector(1, 0, 0), 1.0, Material(
+            Color.from_hex("#00ffff"), ambient=1.0)),
         # Sphere(Vector(1, 0, -2), 0.5, Color.from_hex("#0000ff"))
     ]
-    lights = [Light(position = Vector(1, 1, 1))]
+    lights = [Light(position=Vector(3, 1, 1))]
     scene = Scene(camera, objects, lights, WIDTH, HEIGHT)
 
     engine = RenderEngine()
     img = engine.render(scene)
 
-    # with open("test.ppm", "w") as img_file:
-    #     img.write_ppm(img_file)
     running = True
     while running:
         for event in pg.event.get():
@@ -40,6 +39,7 @@ def main():
 
         # running = False
     pg.quit()
+
 
 if __name__ == "__main__":
     main()
